2efc78a669935884ab9edbdadf448b42b55150ed
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-03-29 15:28:32 +0200

    chore: simplify the snapshot exporter

--- src/main/scala/it/unibo/alchemist/loader/export/exporters/SnapshotExporter.scala
+++ src/main/scala/it/unibo/alchemist/loader/export/exporters/SnapshotExporter.scala
@@ -2,7 +2,6 @@ package it.unibo.alchemist.loader.`export`.exporters
 
 import it.unibo.alchemist.model.interfaces.{Actionable, Environment, Position, Time}
 
-import java.awt.Color
 import java.awt.image.BufferedImage
 
 class SnapshotExporter[T, P <: Position[P]](
@@ -27,50 +26,8 @@ class SnapshotExporter[T, P <: Position[P]](
         java.awt.image.BufferedImage.TYPE_INT_ARGB
       )
       simulationDisplay.paint(image.getGraphics)
-      // snapshots = (image :: snapshots).take(maxSize)
-      // save the image to a file
       javax.imageio.ImageIO.write(image, "png", new java.io.File(path + "/" + name + time.toDouble + ".png"))
-    // processSnapshotIntoOneImage()
     }
-  }
-
-  def processSnapshotIntoOneImage(): Unit = {
-    // for each image, convert the white pixel to transparent
-    val transparentSnapshots = snapshots.reverse.map { snapshot =>
-      for {
-        x <- 0 until snapshot.getWidth
-        y <- 0 until snapshot.getHeight
-      } {
-        val color = new java.awt.Color(snapshot.getRGB(x, y), true)
-        if (color.getRed == 255 && color.getGreen == 255 && color.getBlue == 255) {
-          snapshot.setRGB(x, y, new Color(0, 0, 0, 0).getRGB)
-        }
-      }
-      snapshot
-    }
-    // reduce the alpha value of each pixel with respect to the number of images
-    val alpha = 255 / transparentSnapshots.size
-    transparentSnapshots.zipWithIndex.foreach { case (snapshot, i) =>
-      for {
-        x <- 0 until snapshot.getWidth
-        y <- 0 until snapshot.getHeight
-      } {
-        val color = new java.awt.Color(snapshot.getRGB(x, y), true)
-        if (color.getAlpha != 0) {
-          val newColor = new java.awt.Color(color.getRed, color.getGreen, color.getBlue, alpha * (i + 1))
-          snapshot.setRGB(x, y, newColor.getRGB)
-        }
-      }
-    }
-    // create a new image with the size of the first image
-    val firstImage = transparentSnapshots.head
-    val finalImage = new BufferedImage(firstImage.getWidth, firstImage.getHeight, BufferedImage.TYPE_INT_ARGB)
-    // combined all the images into the final image
-    transparentSnapshots.foreach { snapshot =>
-      finalImage.getGraphics.drawImage(snapshot, 0, 0, null)
-    }
-    // save the final image to a file
-    javax.imageio.ImageIO.write(finalImage, "png", new java.io.File(path + "/" + name + ".png"))
   }
 
   override def close(environment: Environment[T, P], time: Time, l: Long): Unit = {}
